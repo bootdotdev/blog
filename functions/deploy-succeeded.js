21e045711dfbd44b7b719d18b58bda55f185c280
= <lane.c.wagner@gmail.com>
2022-12-02 08:22:53 -0700

    deply success

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -2,7 +2,6 @@ import fetch from "node-fetch";
 const siteMapUrl = "https://blog.boot.dev/sitemap.xml";
 const googleEndpoint = `http://www.google.com/ping?sitemap=${siteMapUrl}`;
 const yandexEndpoint = `https://webmaster.yandex.ru/ping?sitemap=${siteMapUrl}`;
-const bingEndpoint = `http://www.bing.com/webmaster/ping.aspx?siteMap=${siteMapUrl}`;
 
 module.exports.handler = async () => {
   const resp = await fetch(googleEndpoint);
@@ -11,7 +10,5 @@ module.exports.handler = async () => {
   const resp2 = await fetch(yandexEndpoint);
   console.log("Yandex status code:", resp2.status);
 
-  const resp3 = await fetch(bingEndpoint);
-  console.log("Bing status code:", resp3.status);
   return { statusCode: 200, body: "" };
 };
