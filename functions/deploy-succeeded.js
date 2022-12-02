5ded67cafe07d22975b5cb38e633a408b9e0c197
= <lane.c.wagner@gmail.com>
2022-12-02 08:07:59 -0700

    test

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,7 +1,17 @@
-const http = require("http");
+const fetch = require("node-fetch");
 const siteMapUrl = "https://blog.boot.dev/sitemap.xml";
-const endpoint = `http://www.google.com/ping?sitemap=${siteMapUrl}`;
+const googleEndpoint = `http://www.google.com/ping?sitemap=${siteMapUrl}`;
+const yandexEndpoint = `https://webmaster.yandex.ru/ping?sitemap=${siteMapUrl}`;
+const bingEndpoint = `http://www.bing.com/webmaster/ping.aspx?siteMap=${siteMapUrl}`;
 
-module.exports.handler = (event, context, callback) => {
-  http.get(endpoint, () => callback(null, { statusCode: 200, body: "" }));
+module.exports.handler = async () => {
+  const resp = await fetch(googleEndpoint);
+  console.log("Google status code:", resp.status);
+
+  const resp2 = await fetch(yandexEndpoint);
+  console.log("Yandex status code:", resp2.status);
+
+  const resp3 = await fetch(bingEndpoint);
+  console.log("Bing status code:", resp3.status);
+  return { statusCode: 200, body: "" };
 };
