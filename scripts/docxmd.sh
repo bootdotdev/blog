9d50f78ea16a9b06bea86e97bb8bff9c26998daf
wagslane <lane.c.wagner@gmail.com>
2023-02-10 17:06:50 -0700

    back-end dev job description

--- scripts/docxmd.sh
+++ scripts/docxmd.sh
@@ -1,9 +1,16 @@
 # convert
-pandoc -f docx -t markdown_strict --wrap=none -o out.md $1
+pandoc -f docx -t gfm --wrap=none -o out.md $1
 
 # remove stupid underlines
 sed -i '.bak' 's/<u>//g' "out.md"
 sed -i '.bak' 's/<\/u>//g' "out.md"
+
+# remove smart quotes
+sed -i '.bak' 's/“/"/g' "out.md"
+sed -i '.bak' 's/”/"/g' "out.md"
+
+# Fix bullet points
+sed -i '.bak' 's/- /* /g' "out.md"
 
 # no h1s in the article
 sed -i '.bak' 's/^# /## /g' "out.md"
